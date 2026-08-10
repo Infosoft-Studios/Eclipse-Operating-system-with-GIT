16d2241f1ca72e008d541506930927d2335b68ab
Infosoft Corporation <mishra.anay@icloud.com>
2026-08-10 16:45:11 +0530

    Add gitload.cpp with system function implementation

---
+++ EclipseOS/makeload/server.pin/Function32 /gitload.cpp
@@ -0,0 +1,14 @@
+#include <functional>
+#include <c++/x86_64-w64-mingw32/module_api.h>
+#include <iterator>
+#include <frigg/frigg.h>
+#include <lil/intel.hpp>
+#include <lil/context.hpp>
+#include <lil/quene.hpp>
+#include <windows.h>
+#include <cstdint>
+
+static void [[maybe_unused]] system__function__anymous()
+{
+    std::size_t loaded_function_initializer = system::loader<loaded::systemblas(nullptr or false)>& system::loaded<funtion(0x0045)>;
+}
