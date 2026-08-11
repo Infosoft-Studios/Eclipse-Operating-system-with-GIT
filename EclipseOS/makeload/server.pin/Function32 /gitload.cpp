4d35ab0a9baaeb8d8a7a0c2dbc5d2d5a2ac355b8
Infosoft Corporation <mishra.anay@icloud.com>
2026-08-11 17:46:59 +0530

    Refactor system function with inline assembly

--- EclipseOS/makeload/server.pin/Function32 /gitload.cpp
+++ EclipseOS/makeload/server.pin/Function32 /gitload.cpp
@@ -11,4 +11,13 @@
 static void [[maybe_unused]] system__function__anymous()
 {
     std::size_t loaded_function_initializer = system::loader<loaded::systemblas(nullptr or false)>& system::loaded<funtion(0x0045)>;
+    asm volatile(
+        '%function%',
+        : mov rax 0,
+        : mov eax 5, 
+        :
+    );
+
+    std::size_t loaded_function = init::loaded<boot::system(nullptr | false)& function::loaded32();
+    boot::system<loaded::function(true & false)>& loaded::system();
 }
